f442dd07e0f538c106aff364f4b4384bbddb4f63
papposilene <papposilene@users.noreply.github.com>
2019-02-28 20:22:23 +0100

    Add files via upload

---
+++ src/Value/Property/TrackPositionNumberTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MusicBrainz\Value\Property;
+
+use MusicBrainz\Helper\ArrayAccess;
+use MusicBrainz\Value\TrackPositionNumber;
+
+/**
+ * Provides a getter for a track position number.
+ */
+trait TrackPositionNumberTrait
+{
+    /**
+     * The displayed track number
+     *
+     * @var TrackPositionNumber
+     */
+    public $trackPositionNumber;
+
+    /**
+     * Returns the displayed track number.
+     *
+     * @return TrackNumber
+     */
+    public function getTrackPositionNumber(): TrackPositionNumber
+    {
+        return $this->trackPositionNumber;
+    }
+
+    /**
+     * Sets the displayed track position number by extracting it from a given input array.
+     *
+     * @param array $input An array returned by the webservice
+     *
+     * @return void
+     */
+    private function setTrackPositionNumberFromArray(array $input): void
+    {
+        $this->trackPositionNumber = is_null($trackPositionNumber = ArrayAccess::getString($input, 'position'))
+            ? new TrackPositionNumber
+            : new TrackPositionNumber($trackPositionNumber);
+    }
+}
