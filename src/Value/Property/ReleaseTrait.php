dd670a09e252fa919e02d89d9232a5c8df34ee2c
papposilene <philippealexandre.pierre@gmail.com>
2019-03-02 19:24:46 +0100

    Update ReleaseTrait.php

--- src/Value/Property/ReleaseTrait.php
+++ src/Value/Property/ReleaseTrait.php
@@ -2,6 +2,7 @@
 
 namespace MusicBrainz\Value\Property;
 
+use MusicBrainz\Helper\ArrayAccess;
 use MusicBrainz\Value\Release;
 
 /**
@@ -26,4 +27,25 @@ trait ReleaseTrait
         return $this->release;
     }
 
+    /**
+     * Sets the release by extracting it from a given input array.
+     *
+     * @param array $input An array returned by the webservice
+     * @param null|string $key   Optional array key (default: "release")
+     *                           Use first array dimension for null
+     *
+     * @return void
+     */
+    private function setReleaseFromArray(array $input, ?string $key = 'release'): void
+    {
+        if (is_null($key)) {
+            $this->release = new Release($input);
+
+            return;
+        }
+
+        $this->release = is_null($release = ArrayAccess::getArray($input, $key))
+            ? new Release
+            : new Release($release);
+    }
 }
