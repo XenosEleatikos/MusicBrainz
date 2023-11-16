9ed635b13522db367872b114e2801eab88dc2913
Christoph Hochholzer <christoph@hochholzer.info>
2023-11-17 00:27:34 +0100

    Fix fields for browse series

--- src/Supplement/Browse/SeriesFields.php
+++ src/Supplement/Browse/SeriesFields.php
@@ -8,6 +8,8 @@ class SeriesFields extends Fields
 {
     use \MusicBrainz\Supplement\Field\AliasesTrait;
     use \MusicBrainz\Supplement\Field\AnnotationTrait;
+    use \MusicBrainz\Supplement\Field\GenresTrait;
     use \MusicBrainz\Supplement\Field\TagsTrait;
+    use \MusicBrainz\Supplement\Field\UserGenresTrait;
     use \MusicBrainz\Supplement\Field\UserTagsTrait;
 }
