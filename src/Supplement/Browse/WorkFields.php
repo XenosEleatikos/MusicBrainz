572c16f97341d0c3147dcb5f1097c7e8988ec179
Christoph Hochholzer <christoph@hochholzer.info>
2023-11-17 00:34:02 +0100

    Fix fields for browse work

--- src/Supplement/Browse/WorkFields.php
+++ src/Supplement/Browse/WorkFields.php
@@ -8,8 +8,10 @@ class WorkFields extends Fields
 {
     use \MusicBrainz\Supplement\Field\AliasesTrait;
     use \MusicBrainz\Supplement\Field\AnnotationTrait;
+    use \MusicBrainz\Supplement\Field\GenresTrait;
     use \MusicBrainz\Supplement\Field\RatingsTrait;
     use \MusicBrainz\Supplement\Field\TagsTrait;
+    use \MusicBrainz\Supplement\Field\UserGenresTrait;
     use \MusicBrainz\Supplement\Field\UserRatingsTrait;
     use \MusicBrainz\Supplement\Field\UserTagsTrait;
 }
