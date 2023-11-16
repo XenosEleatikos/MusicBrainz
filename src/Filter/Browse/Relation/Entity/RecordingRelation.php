a1c929e5d1d19dd73005a585b086a10df9cdce37
Christoph Hochholzer <christoph@hochholzer.info>
2023-11-17 00:48:44 +0100

    Fix fields and relations for browse recording

--- src/Filter/Browse/Relation/Entity/RecordingRelation.php
+++ src/Filter/Browse/Relation/Entity/RecordingRelation.php
@@ -12,4 +12,5 @@ class RecordingRelation extends AbstractRelation
     use \MusicBrainz\Filter\Browse\Relation\RelatedEntity\ArtistTrait;
     use \MusicBrainz\Filter\Browse\Relation\RelatedEntity\CollectionTrait;
     use \MusicBrainz\Filter\Browse\Relation\RelatedEntity\ReleaseTrait;
+    use \MusicBrainz\Filter\Browse\Relation\RelatedEntity\WorkTrait;
 }
--- src/Supplement/Browse/RecordingFields.php
+++ src/Supplement/Browse/RecordingFields.php
@@ -9,8 +9,10 @@ class RecordingFields extends Fields
     use \MusicBrainz\Supplement\Field\AnnotationTrait;
     use \MusicBrainz\Supplement\Field\ArtistCreditsTrait;
     use \MusicBrainz\Supplement\Field\IsrcsTrait;
+    use \MusicBrainz\Supplement\Field\GenresTrait;
     use \MusicBrainz\Supplement\Field\RatingsTrait;
     use \MusicBrainz\Supplement\Field\TagsTrait;
+    use \MusicBrainz\Supplement\Field\UserGenresTrait;
     use \MusicBrainz\Supplement\Field\UserRatingsTrait;
     use \MusicBrainz\Supplement\Field\UserTagsTrait;
 }
