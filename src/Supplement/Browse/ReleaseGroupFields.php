94347c10cd77a20356007e1196d5402f03052c05
Christoph Hochholzer <christoph@hochholzer.info>
2023-11-19 10:50:47 +0100

    Fix include fields for browse release group

--- src/Supplement/Browse/ReleaseGroupFields.php
+++ src/Supplement/Browse/ReleaseGroupFields.php
@@ -8,8 +8,10 @@ class ReleaseGroupFields extends Fields
 {
     use \MusicBrainz\Supplement\Field\AnnotationTrait;
     use \MusicBrainz\Supplement\Field\ArtistCreditsTrait;
+    use \MusicBrainz\Supplement\Field\GenresTrait;
     use \MusicBrainz\Supplement\Field\RatingsTrait;
     use \MusicBrainz\Supplement\Field\TagsTrait;
+    use \MusicBrainz\Supplement\Field\UserGenresTrait;
     use \MusicBrainz\Supplement\Field\UserRatingsTrait;
     use \MusicBrainz\Supplement\Field\UserTagsTrait;
 }
